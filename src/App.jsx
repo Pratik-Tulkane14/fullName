d5b93a21ce63af1d5017d3e32efe25b3f7724674
Pratik-Tulkane14 <pratiktulkane12345@gmail.com>
2025-03-17 21:12:26 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [isShow, setIsShow] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsShow((prev) => !prev);
+  };
+  return (
+    <main className="main">
+      <h1>Full Name Display</h1>
+      <form onSubmit={handleSubmit} className="form">
+        <div className="first-section">
+          <label htmlFor="FName">First Name:</label>
+          <input
+            id="FName"
+            type="text"
+            required
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+        </div>
+        <div className="second-section">
+          <label htmlFor="LName">Last Name:</label>
+          <input
+            id="LName"
+            type="text"
+            required
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="submit-btn">
+          Submit
+        </button>
+        {isShow ? (
+          <p>
+            {firstName},{lastName}
+          </p>
+        ) : null}
+      </form>
+    </main>
+  );
+}
+
+export default App;
